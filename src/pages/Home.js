8b155b498dc3d0dfbc4cef3e309462a62165deb2
HAKIT1994 <hakitblack@gmail.com>
2022-07-29 00:03:19 +0800

    Further ideas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,17 @@ import './Home.css'
 import { auth, db} from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
-import {updateProfile} from 'firebase/auth'
+import { updateProfile} from 'firebase/auth'
 
 function Home() {
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
-  const photoURL = user.photoURL;
-  const [selectedImage, setSelectedImage] = useState(null);
+  const displayName = (user||[]).displayName;
+  // const [changename,setChangename] = useState()
+
 
   if (user){var Emailname = user.email.split("@")[0]}
+  // if (displayName){Emailname=displayName}
 
   useEffect(() => {
     if (loading) {
@@ -21,50 +23,34 @@ function Home() {
     if (!user) return navigate("/login");
   }, [user, loading]);
   
-  function uploadtofirebase(img) {
-    console.log(img)
-    console.log(user.displayName)
-    updateProfile(user, {
-      displayName: "Jane Q. User", photoURL: img
-    }).then(() => {
-      // Profile updated!
-      console.log(photoURL)
-      // ...
-    }).catch((error) => {
-      // An error occurred
-      // ...
-    });
-  }
+
+  // function ChName(){
+  //   updateProfile(user, {
+  //     displayName: changename
+  //   }).then(() => {
+  //     return navigate("/home");
+  //   }).catch((error) => {
+  //     alert(error.code)
+  //     // An error occurred
+  //     // ...
+  //   });
+  // }
 
   return (
+    
     <>
     <div className='home'>
       <div className='home__msg'>
           <h2>Welcome {Emailname}</h2>
-          
-          <div>
-          <h2>Upload and Display Image </h2>
-          {selectedImage && (
-            <div>
-            <img alt="not fount" width={"250px"} className='upload__img' src={URL.createObjectURL(selectedImage)} />
-            <br />
-            <button onClick={()=>setSelectedImage(null)}>Remove</button>
-            </div>
-          )}
-          <br />
-          <br /> 
-          <input type="file" name="myImage" onChange={(event) => {
-              console.log(event.target.files[0]);
-              setSelectedImage(event.target.files[0]);
-              
-          }}/>
-          <button type="button" onclick={uploadtofirebase(selectedImage)}>upload</button>
-          
-          {photoURL}
-          {console.log(auth.currentUser)}
-          
-    </div>
-      </div>
+          <div className='user__option'>
+          {/* <table className='user__option__item'>
+            <tr>
+              <td><input type='text' placeholder={Emailname} onChange={(e) => setChangename(e.target.value)} /></td>
+              <td><button className="chat_btn" onClick={ChName}>Update User Name</button></td>
+            </tr>
+          </table> */}
+          </div>
+        </div>
     </div>
     </>
   )
